0d1f2b13d1667fa93f79763ba70908712de81f65
Erik Shandrowski <erik843@gmail.com>
2014-06-23 14:56:00 -0600

    Finished?

--- main.js
+++ main.js
@@ -6,4 +6,43 @@ while(phoneNumber.charAt(3) !== '-' && phoneNumber.charAt(7) !== '-') {
 }
 	alert("Thank you! Your phone number is valid.");
 
+var birthDate = prompt("Please enter your birth date: \nxx/xx/xx");
 
+while(birthDate.charAt(2) !== '/' && birthDate.charAt(5) !== '/') {
+	birthDate = prompt("The date you entered is invalid.\n" +  
+			"Please enter in the following format: xx/xx/xx");	
+}
+	console.log("Thank you! Your birth date is valid.");
+
+var postalCode = prompt("Please enter your postal code: \nxxxxx OR xxxxx-xxxx");
+
+if(postalCode.charAt(5) === '-' || postalCode.charAt(5) === '') {
+	console.log("Thank you! Your postal code is valid.");
+}
+else {
+	postalCode = prompt("Invalid postal code. Please enter your postal code: \nxxxxx OR xxxxx-xxxx");
+}
+
+var state = prompt("Please enter your state. Two letter code only: \n XX").toUpperCase();
+
+while(state.length !== 2) {
+	prompt("You did not enter a valid state code. \nPlease enter your state. Two letter code only: \n XX");
+}
+
+console.log("Your state code is valid.");
+
+var married = prompt("Are you married? Yes or no only").toLowerCase();
+
+if(married === 'yes' || married === 'no') {
+	console.log("Your response was valid. Thanks!");
+}
+else {
+	var married = prompt("Not a valid response. Are you married? Yes or no only").toLowerCase();	
+}
+
+console.log("Here is the info you entered: \n\t Phone Number: " + phoneNumber + 
+	"\n\t Birth Date: " + birthDate + 
+	"\n\t Postal code: " + postalCode + 
+	"\n\t State: " + state + 
+	"\n\t Married: " + married +
+	"\n Thanks!");
